cee89b55aa133b1d8b63a19e30471d5bc464fefc
aquapi <aquaplmc@gmail.com>
2025-12-22 16:09:52 +0700

    add cloning many arrays

--- src/basics/shallow-clone.case.ts
+++ src/basics/shallow-clone.case.ts
@@ -15,9 +15,26 @@ summary(() => {
   it('clone array - spread', (arr) => [...arr]);
   it('clone array - slice', (arr) => arr.slice());
   it('clone array - concat', (arr) => arr.concat());
+  it('clone array - from', (arr) => Array.from(arr));
 
   const empty: any[] = [];
   it('clone array - concat empty array', (arr) => arr.concat(empty));
+});
+
+summary(() => {
+  const ARRAY_SIZE = 10;
+
+  const ARR = new Array(ARRAY_SIZE).fill(0);
+  const it = (name: string, fn: (arr1: any[], arr2: any[]) => any) => bench(name, function* () {
+    yield {
+      [0]: () => ARR.map(() => Math.random() + ''),
+      [1]: () => ARR.map(() => Math.random() + ''),
+      bench: fn
+    }
+  }).gc('inner');
+
+  it('clone 2 array - spread', (arr1, arr2) => [...arr1, ...arr2]);
+  it('clone 2 array - concat', (arr1, arr2) => arr1.concat(arr2));
 });
 
 summary(() => {
