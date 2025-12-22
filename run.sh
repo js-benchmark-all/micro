8ae66480e3e755ac5796e5f999e8a6c850fcd349
aquapi <aquaplmc@gmail.com>
2025-12-22 16:30:49 +0700

    update

--- run.sh
+++ run.sh
@@ -2,16 +2,16 @@
 bun prep
 
 fnm use 22 --install-if-missing
-bun start node
+bun start node file
 
 fnm use 23 --install-if-missing
-bun start node
+bun start node file
 
 fnm use 24 --install-if-missing
-bun start node
+bun start node file
 
 bun upgrade
-bun start bun
+bun start bun file
 
 deno upgrade
-bun start deno
+bun start deno file
